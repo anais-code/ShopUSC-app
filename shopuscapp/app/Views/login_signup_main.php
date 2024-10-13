f43d52fc4d1b61d18fc373cff0b89670ad3b574f
Michelle Douglas <miche.doug29@gmail.com>
2024-10-13 12:10:01 -0400

    Added sign up and log in page and styles

--- shopuscapp/app/Views/login_signup_main.php
+++ shopuscapp/app/Views/login_signup_main.php
@@ -2,22 +2,25 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Shop USC - Sign Up / Log In</title>
         <link rel="icon" type="image/png" href="/assets/imgs/USC-7 Background Removed.png">
-        <title>Shop USC</title>
         <link rel="stylesheet" href="/assets/styles/styles.css">
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&family=Raleway:wght@400;600&display=swap" rel="stylesheet">
     </head>
     <body class="body-lsm">
         <header class="header-lsm">
-        <a href="<?= base_url('homepage'); ?>">
-            <img src="<?= base_url('assets/imgs/USC-7-removebg-preview.png'); ?>" alt="Shop USC logo" class="logo" style="position: fixed; top: 1em; left: 1em; height: 5em; z-index: 3;">
+        <a href="<?= base_url('homepage'); ?>"> <!-- Link to homepage -->
+            <img src="/assets/imgs/USC-7-removebg-preview.png" alt="Shop USC logo" class="logo" style="position: fixed">
         </a>
+</header>
+
         <main class="main-lsm">
-            <div>
-                <h1>Shop USC</h1>
-            </div>
-            <p>Here is some text for our app.</p>
+            <h2>SIGN UP /LOG IN</h2> <!-- Title for the page -->
+            <div class= "button-container">
+                <a href="<?= base_url('buyer_signup') ?>" class="buyer-btn">Buyer</a>
+                <a href="<?= base_url('seller_signup') ?>" class="seller-btn">Seller</a>
+</div>
         </main>
 
         <footer class="footer-lsm">
